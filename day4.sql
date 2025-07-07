bee6ea99bba4a14c22c5ed025af526c422f7ce39
Gani25 <memonabdulgani3d@gmail.com>
2025-07-07 21:00:43 +0530

    Update day4.sql

--- day4.sql
+++ day4.sql
@@ -51,3 +51,35 @@ insert into student(first_name, last_name, roll_no) values
 ("Hritik","Sharma",5);
 
 select * from student;
+
+/* Update/Delete*/
+
+drop table student;
+
+/*Constraints*/
+create table student
+(
+	roll_no int primary key auto_increment,
+    first_name varchar(20) not null,
+    last_name varchar(20) not null,
+    email varchar(45) unique not null,
+    phone varchar(15) unique not null,
+    acc_created_on timestamp not null default now()
+);
+ 
+describe student;
+insert into student(first_name,last_name, email,phone)
+values
+("Prakash","Jha","prakash@gmail.com", "8585859585");
+insert into student(first_name,last_name, email,phone)
+values
+("Prakash","Jha",null,null);
+
+select * from student;
+insert into student(first_name,last_name, email,phone)
+values
+("Rakesh","Jha","rakesh@gmail.com", "9898989898");
+
+select * from student;
+
+show create table student;
