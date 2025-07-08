3af3df930a98a5587febf94324c85009f441050c
Gani25 <memonabdulgani3d@gmail.com>
2025-07-08 20:02:01 +0530

    Create day5.sql

---
+++ day5.sql
@@ -0,0 +1,33 @@
+use sprk_weekdays;
+
+show tables;
+describe student;
+
+select * from student;
+
+insert into student values
+(10,"Ashutosh","Verma","ashu@gmail.com",
+"9858857585","2025-01-25"),
+(15,"Pranjal","Gupta","pranjal@gmail.com",
+"9858581200",default);
+
+select * from student;
+insert into student values
+(default,"Abdul","Memon","abdu@gmail.com",
+"1245124512",default);
+
+select * from student;
+
+insert into student(roll_no,email,first_name, last_name, phone)
+ values
+(2,"soham@gmail.com", "Soham","Deshmukh",
+"4574859568");
+
+select * from student;
+
+insert into student(email,first_name, last_name, phone)
+ values
+("shruti@gmail.com", "Shruti","Sharma",
+"4852100255");
+
+select * from student;
