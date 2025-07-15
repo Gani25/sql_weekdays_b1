e31babca0c19822f3a25262dea6d2c6e43ffdd9a
Gani25 <memonabdulgani3d@gmail.com>
2025-07-15 20:36:53 +0530

    Update day8.sql

--- day8.sql
+++ day8.sql
@@ -56,3 +56,51 @@ t2.id T2ID, t2.value T2Value
 from my_table_one t1
 join my_table_two t2
 on t1.value = t2.value;
+
+
+select t1.id T1ID, t1.value T1Value,
+t2.id T2ID, t2.value T2Value 
+from my_table_one t1
+left join my_table_two t2
+on t1.id = t2.id;
+
+select t1.id T1ID, t1.value T1Value,
+t2.id T2ID, t2.value T2Value 
+from my_table_one t1
+left join my_table_two t2
+on t1.value = t2.value;
+
+
+select t1.id T1ID, t1.value T1Value,
+t2.id T2ID, t2.value T2Value 
+from my_table_one t1
+right join my_table_two t2
+on t1.value = t2.value;
+
+
+select * from student;
+select * from course;
+
+insert into student values
+(2,"Pranjal","181818",1010),
+(1,"Akash","121212",1008),
+(2,"Pranjal","181818",1001);
+
+
+select * from student;
+select * from course;
+
+select * from student s 
+inner join course c 
+on s.course_id = c.c_id
+order by roll_no;
+
+select s.*, c.c_name, c.c_duration from student s 
+inner join course c 
+on s.course_id = c.c_id
+order by roll_no;
+
+select s.*, c.c_name, c.c_duration from student s 
+left join course c 
+on s.course_id = c.c_id
+order by roll_no;
