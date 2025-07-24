d9f4f43c66d2e81adbed339aa33446e073f70b79
Gani25 <memonabdulgani3d@gmail.com>
2025-07-24 20:21:16 +0530

    Create day13.sql

---
+++ day13.sql
@@ -0,0 +1,84 @@
+-- day 13
+
+use sprk_weekdays;
+
+select * from employee;
+
+-- Calculate Age
+select *,timestampdiff(year, dob, now()) age from employee;
+
+select *,year(now())- year(dob) age from employee;
+
+
+select *,timestampdiff(year, dob, now()) age from employee;
+
+-- Find all employees whose age is between 30 to 40
+select *,timestampdiff(year, dob, now()) age from employee
+having age between 30 and 40;
+
+-- subquery in from clause
+
+select * from 
+(select *,timestampdiff(year, dob, now()) age 
+from employee) emp_with_age
+where age between 30 and 40;
+
+-- find all employee who earns more than average 
+-- salary in company
+select * from employee
+where salary > (select avg(salary) from employee);
+
+-- Correlated Subquery
+-- Find All Employees who earns more than average 
+-- salary in their department
+select dept_name, avg(salary) from employee
+group by dept_name;
+
+
+select * from employee;
+select avg(salary) from employee e2
+where e2.dept_name = "Sales";
+
+select * from employee e1
+where salary >
+	(
+		select avg(salary) from employee e2
+        where e2.dept_name = e1.dept_name
+	);
+
+-- Corelated Subquery
+-- Subquery in select statement as a column
+use classicmodels;
+select * from customers;
+select customerNumber, customerName,
+concat_ws(" ", contactFirstName, contactLastName) contactName
+ from customers;
+select * from orders;
+select * from orderdetails;
+select count(*) from orders
+where customerNumber = 319;
+
+
+select customerNumber, customerName,
+concat_ws(" ", contactFirstName, contactLastName) contactName,
+	(
+		select count(*) 
+		from orders o 
+		where o.customerNumber = c.customerNumber
+	) order_count
+ from customers c;
+ 
+-- Find customer with 0 orders
+ 
+select * from
+(
+	select customerNumber, customerName,
+	concat_ws(" ", contactFirstName, contactLastName) contactName,
+		(
+			select count(*) 
+			from orders o 
+			where o.customerNumber = c.customerNumber
+		) order_count
+	 from customers c
+) cust_order_count
+where order_count =0;
