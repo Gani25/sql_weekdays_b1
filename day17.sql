293d430d2290756804683c3584bd73b490ef0c0f
Gani25 <memonabdulgani3d@gmail.com>
2025-08-01 20:48:25 +0530

    Update day17.sql

--- day17.sql
+++ day17.sql
@@ -29,6 +29,7 @@ select order_id, customer_id, getName(customer_id) as name, status from orders;
 show tables;
 select * from order_items;
 
+
 select o.order_id, customer_id, getName(customer_id) as name, status,
 o_it.* 
 from orders o
@@ -59,5 +60,57 @@ join order_items o_it
 on o. order_id = o_it.order_id;
 
 
-select * from order_items;
+select * from order_items
+where order_id = 1004;
 select * from orders;
+
+delimiter $
+drop function if exists count_products $
+
+create function count_products(ord_id int)
+returns int 
+deterministic
+begin
+	declare prod_count int;
+    select count(*) into prod_count from order_items
+    where order_id = ord_id;
+    
+    return prod_count;
+end $
+delimiter ;
+
+select count_products(1004);
+select *, count_products(order_id) product_count from orders;
+
+
+-- Group concat
+select * from products;
+select distinct category from products;
+select category, count(*) prod_count, name
+ from products
+group by category; -- ERROR / Bad Data
+select category, count(*) prod_count, group_concat(name separator ", ") as product_names
+ from products
+group by category;
+
+use sakila;
+
+show tables;
+select * from film_actor ;
+select * from actor;
+select * from film;
+
+select film_id,
+group_concat(concat_ws(" ", a.first_name, a.last_name) separator ", ") 
+actor_names
+from actor a
+join film_actor fa on a.actor_id = fa.actor_id
+group by film_id;
+
+select fa.film_id, title,
+group_concat(concat_ws(" ", a.first_name, a.last_name) separator ", ") 
+actor_names
+from actor a
+join film_actor fa on a.actor_id = fa.actor_id
+join film f on fa.film_id = f.film_id
+group by fa.film_id;
