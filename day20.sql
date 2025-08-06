42ba616085a6f1884ed5b3816f125cb52c4c76f8
Gani25 <memonabdulgani3d@gmail.com>
2025-08-06 20:45:07 +0530

    Update day20.sql

--- day20.sql
+++ day20.sql
@@ -12,9 +12,35 @@ add email varchar(50) unique after dept_name;
 desc employee;
 
 show index from employee;
-select * from employee;
+
+explain
+select * from employee
+where emp_id = 23;
+explain analyze
+select * from employee
+where emp_id = 23;
+
+select * from employee
+where dept_name = "IT";
+
+explain select * from employee
+where dept_name = "IT";
+explain analyze select * from employee
+where dept_name = "IT";
+
 
 create index idx1 on employee(dept_name,dob);
+create index idx1 on employee(dept_name);
+
+select * from employee
+where dept_name = "IT";
+
+explain select * from employee
+where dept_name = "IT";
+explain analyze select * from employee
+where dept_name = "IT";
+
+
 create unique index idx2 on employee(name);
 
 show index from employee;
@@ -54,3 +80,47 @@ select * from employee;
 alter table employee
 modify emp_id int auto_increment;
 desc employee;
+
+explain select * from employee where emp_id = 20;
+explain select * from employee where name = "Pranjal Sharma" ;
+explain analyze select * from employee where salary = 3500;
+explain analyze select * from employee where dept_name = "IT";
+
+-- Exception Handling
+show tables;
+delimiter $
+drop procedure if exists get_all $
+create procedure get_all()
+begin
+	declare exit handler for 1146 
+    select "Table not Found" as errorMessage;
+
+	select * from employee;
+	select * from customers;
+	select * from ordrs;
+	select * from order_items;
+	select * from products;
+    
+end $
+delimiter ;
+
+call get_all();
+
+delimiter $
+drop procedure if exists get_all $
+create procedure get_all()
+begin
+	declare continue handler for 1146 
+    select "Table not Found" as errorMessage;
+
+	select * from demo;
+	select * from employee;
+	select * from customers;
+	select * from ordrs;
+	select * from order_items;
+	select * from products;
+    
+end $
+delimiter ;
+
+call get_all();
